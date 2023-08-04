a9a9e553f738f5b012fb98e993abd08533f0e255
Usman Tahir <usman.tahir@datumbrain.com>
2023-08-04 12:58:24 +0500

    Revert "Removing Redundancy from the Service"
    This reverts commit 24feeebadf937f84a992892ee93572561662fbad.

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/takama/daemon"
 )
 
 const port = ":8769"
@@ -26,7 +28,68 @@ func main() {
 		}
 	}()
 
-	// Run the HTTP server continuously
+	if len(os.Args) > 1 {
+		cmd := os.Args[1]
+		switch cmd {
+		case "install":
+			d, err := daemon.New(serviceName, "Your daemon description", daemon.SystemDaemon)
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			_, err = d.Install()
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			log.Println("Daemon installed successfully.")
+			os.Exit(0)
+		case "remove":
+			d, err := daemon.New(serviceName, "Your daemon description", daemon.SystemDaemon)
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			_, err = d.Remove()
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			log.Println("Daemon removed successfully.")
+			os.Exit(0)
+		case "start":
+			d, err := daemon.New(serviceName, "Your daemon description", daemon.SystemDaemon)
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			_, err = d.Start()
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			log.Println("Daemon started successfully.")
+			os.Exit(0)
+		case "stop":
+			d, err := daemon.New(serviceName, "Your daemon description", daemon.SystemDaemon)
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			_, err = d.Stop()
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			log.Println("Daemon stopped successfully.")
+			os.Exit(0)
+		case "status":
+			d, err := daemon.New(serviceName, "Your daemon description", daemon.SystemDaemon)
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			status, err := d.Status()
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			log.Printf("Daemon status: %v", status)
+			os.Exit(0)
+		}
+	}
+
+	// This part will be executed when running the binary normally without any command.
 	server()
 
 	done <- true
